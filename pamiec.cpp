766d10c22dedca1f962fde4793764b02a8f09a58
nataliapop <nataliapop@MY-DESKTOP>
2016-10-20 18:29:26 +0200

    pola klasy file

--- pamiec.cpp
+++ pamiec.cpp
@@ -1,1 +1,14 @@
-//
+#include <iostream>
+#include <string>
+using std::string;
+class file
+{
+private:
+	string file_name;
+	string file_type;
+	//location;
+	int size;
+	//protection;
+	int acces_time;
+
+};
