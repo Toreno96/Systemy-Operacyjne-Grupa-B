e60d3510d671a97f29b72b1511eb3740af888200
nataliapop <nataliapop@MY-DESKTOP>
2016-11-02 21:21:12 +0100

    nothing

--- system plikow.cpp
+++ system plikow.cpp
@@ -14,9 +14,8 @@ private:
 	std::array <int, 5> sectorID; // wartosc ujemna – nieprzypisany sektor do pliku​
 
 public:
-	FCB(std::array <char, 8> filename_, std::array <char, 3> type_, std::array <int, 5> sectorID_) : filename(filename_), type(type_), sectorID(sectorID_)
-	{}
-	std::array <int, 5> read_sectors() {}
+	FCB(std::array <char, 8> filename_, std::array <char, 3> type_, std::array <int, 5> sectorID_) : filename(filename_), type(type_), sectorID(sectorID_) {}
+	std::array <int, 5> read_sectors () {}
 };
 
 class Sector
@@ -26,7 +25,7 @@ private:
 	std::array <char, 16> data;
 
 public:
-	bool save_data(std::array <char, 16> data_) {} // 1 – operacja zakonczona pomyslnie
+	bool save_data (std::array <char, 16> data_) {} // 1 – operacja zakonczona pomyslnie
 	std::array <char, 16> read_data() {}
 	std::array <bool, 16> get_bitvector() {}
 };
@@ -35,15 +34,15 @@ class HardDrive
 {
 private:
 	std::list <FCB> Catalog{};
-	Sector harddrive[16];
+	Sector harddrive [16];
 	unsigned int find_empty_sector() {}
 
 public:
-	bool create_file(std::array <char, 8> filename_, std::array <char, 3> type_) {} // 1 – operacja zakonczona pomyslnie
-	bool delete_file(std::array <char, 8> filename_, std::array <char, 3> type_) {}
-	FCB read_file(std::array <char, 8> filename_, std::array <char, 3> type_) {}
-	bool add_to_file(std::array <char, 8> filename_, std::array <char, 3> type_, std::array <char, 16> data_) {}
+	bool create_file (std::array <char, 8> filename_, std::array <char, 3> type_) {} // 1 – operacja zakonczona pomyslnie
+	bool delete_file (std::array <char, 8> filename_, std::array <char, 3> type_) {}
+	FCB read_file (std::array <char, 8> filename_, std::array <char, 3> type_) {}
+	bool add_to_file (std::array <char, 8> filename_, std::array <char, 3> type_, std::array <char, 16> data_) {}
 
-	std::array <char, 16> read_sector(unsigned int sectorID) {}
-	std::list <FCB> get_file_list() {}
+	std::array <char, 16> read_sector (unsigned int sectorID) {}
+	std::list <FCB> get_file_list () {}
 };
