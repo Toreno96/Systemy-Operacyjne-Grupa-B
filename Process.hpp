96a5097b57b006d958bad44a354d45f6d046a377
Toreno96 <toreno96@gmail.com>
2016-11-29 00:43:14 +0100

    Dodanie getterów i setterów
    A także kilku nowych notatek skierowanych głównie do samego mnie,
    związanych między innymi z powyższym.

--- Process.hpp
+++ Process.hpp
@@ -10,7 +10,18 @@ class Process {
     // sugestie, dotycz¹ce zmiany nazw czy umieszczenia dodatkowych stanów.
     enum class State { New, Ready, Running, Waiting, Terminated };
     Process( std::string name );
-    // Poza tym - helluva getterów i setterów...
+    std::string getName() const;
+    unsigned int getOriginalPriority() const;
+    unsigned int getCurrentPriority() const;
+    State getState() const;
+    Undefined getPageTable() const;
+    // Getter(y?) dla rejestrów
+    Undefined getInstructionCounter() const;
+    void setCurrentPriority( unsigned int priority );
+    void setState( State state ); // Zamiast tego powrót do koncepcji osobnych
+    // metod do ustawiania ka¿dego stanu i wykonania "ewentualnych dodatkowych, zwi¹zanych z tym operacji"?
+    // Settery dla rejestrów
+    void setInstructionCounter( Undefined instructionCounter );
   protected:
     // Nazwa procesu, a jednoczeœnie i jego identyfikator
     std::string name_;
@@ -21,7 +32,7 @@ class Process {
     // Tablica stron, w której znajduj¹ siê informacje o ulokowaniu kodu
     // programu.
     Undefined pageTable;
-    // Rejestry. Ile ich?
+    // Rejestry. Ile ich? Mo¿e lepiej zrobiæ z tego tablicê?
     Undefined A_, B_, C_;
     // Licznik rozkazów. Zamieniæ na typ int? Nazwê na programCounter/inn¹?
     Undefined instructionCounter_;
