b31e0ea62c0b22c8d436e109b6e3431675b82404
Toreno96 <toreno96@gmail.com>
2016-11-29 01:24:49 +0100

    Dodanie zaczątków implementacji klasy Process

---
+++ Process.cpp
@@ -0,0 +1,30 @@
+#include "Process.hpp"
+
+// Process::Process( const std::string& name, const Undefined& pageTable ) : Process::name_( name ), Process::pageTable_( pageTable )
+std::string Process::getName() const {
+  return name_;
+}
+unsigned int Process::getOriginalPriority() const {
+  return originalPriority_;
+}
+unsigned int Process::getCurrentPriority() const {
+  return currentPriority_;
+}
+Process::State Process::getState() const {
+  return state_;
+}
+Undefined Process::getPageTable() const {
+  return pageTable_;
+}
+Undefined Process::getInstructionCounter() const {
+  return instructionCounter_;
+}
+void Process::setCurrentPriority( unsigned int priority ) {
+  currentPriority_ = priority;
+}
+void Process::setState( const Process::State& state ) {
+  state_ = state;
+}
+void Process::setInstructionCounter( Undefined instructionCounter ) {
+  instructionCounter_ = instructionCounter;
+}
