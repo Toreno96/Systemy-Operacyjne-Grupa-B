7d94689e3934f5ef0c6f7f8dd85c02250e2f85a3
Toreno96 <toreno96@gmail.com>
2016-11-29 01:20:02 +0100

    Zmiana argumentów konstruktora klasy Process

--- Process.hpp
+++ Process.hpp
@@ -8,7 +8,7 @@ class Process {
     // Nazwy stanów na podstawie materia³ów z wa¿niaka. Jestem otwarty na wszelkie
     // sugestie, dotycz¹ce zmiany nazw czy umieszczenia dodatkowych stanów.
     enum class State { New, Ready, Running, Waiting, Terminated };
-    Process( std::string name );
+    Process( const std::string& name, const Undefined& pageTable );
     std::string getName() const;
     unsigned int getOriginalPriority() const;
     unsigned int getCurrentPriority() const;
