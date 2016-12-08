307fb0929aea5d2df70af3b7f29081a02e114eeb
Roski96 <oski0927@gmail.com>
2016-12-08 18:16:02 +0100

    Add files via upload
    poprawiona wersja zamków:
    -dziala referencja do procesow,
    -odpowiednio odblokowuje wraz z weryfikacja procesu,
    -w innym wypadku wyrzuca blad,
    -dziala tez wszelkie wypisywanie:
      +ilosci procesow pod zamkiem,
      +wypis wszystkich procesow pod zamkiem

---
+++ Locks.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<deque>
+#include<string>
+#include"Process.hpp"
+#include"Undefined.hpp"
+#include"Locks.hpp"
+void Lock::show_list_of_processes()
+{
+	std::cout << "Names of processes under lock: " << std::endl;
+	int i = 1;
+	for (auto process : waiting_process_list)
+	{
+		std::cout << i << ". " << process.get().getName() << std::endl;
+		i++;
+	}
+}
+std::string Lock::GetProcessName()
+{
+		std::cout << "Name of first process under lock: " << blocking_IDs_list.front() << std::endl;
+		return blocking_IDs_list.front();
+}
+int Lock::GetValue()
+{
+		std::cout << "Number of processes in queue: " << abs(value) << std::endl;
+		return abs(value);
+}
+void Lock::lock(Process& process)
+{
+
+	value--;
+
+	if (value < 0)
+	{
+		waiting_process_list.push_back(std::ref(process));
+		blocking_IDs_list.push_back(process.getName());
+		process.setState(Process::State::Waiting); 
+	}
+}
+void Lock::unlock(Process &process)
+{
+		if (blocking_IDs_list[0] == process.getName())
+		{
+			if (value < 1)
+			{
+				value++;
+			}
+			if (value <= 0)
+			{
+				waiting_process_list.pop_front();
+				blocking_IDs_list.pop_front();
+				process.setState(Process::State::Ready); 
+			}
+		}
+		else
+		{
+			std::cout << "Error" << std::endl;
+		}
+}
---
+++ Locks.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+#include"Process.hpp"
+#include<deque>
+class Lock
+{
+protected:
+	int value = 1;			 // wartosc zamka, ujemne wartosci oznaczaja ile procesow czeka pod zamkiem
+	std::deque <std::reference_wrapper<Process>> waiting_process_list; //lista procesow czekajacych pod zamkiem
+	std::deque <std::string> blocking_IDs_list; //lista ID procesow czekajacych pod zamkiem, uzywana do weryfikacji procesu, ktory zajal zamek
+public:
+	int GetValue(); // ilosc procesow pod zamkiem
+	std::string GetProcessName(); //wypisuje id procesu pierwszego do zwolnienia
+	void show_list_of_processes(); // wyswietlenie calej listy procesow pod zamkiem
+	void lock(Process &process);   // metoda ryglujaca
+	void unlock(Process &process); // metoda otwierajaca zamek, otworzy jesli ID procesu zgadza sie z ID procesu, ktory zamknal zamek
+};
