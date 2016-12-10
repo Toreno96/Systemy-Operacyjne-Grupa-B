5057c4819e6fd83628a10e5da181b931165289bb
Toreno96 <toreno96@gmail.com>
2016-12-11 00:29:54 +0100

    Wstępny (?) test klasy Process

---
+++ tests/helpers.hpp
@@ -0,0 +1,5 @@
+#include "../src/Process.hpp"
+
+// Potencjalna dodatkowa metoda klasy Process?
+std::string convertProcessStateToString( Process::State state );
+void printProcessData( const Process& process );
