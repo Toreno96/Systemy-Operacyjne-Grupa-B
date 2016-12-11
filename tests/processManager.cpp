d4fee3bdc9da8553174c808b276ab4c29c1301a3
Toreno96 <toreno96@gmail.com>
2016-12-11 01:07:29 +0100

    Dokładniejsze testy

--- tests/processManager.cpp
+++ tests/processManager.cpp
@@ -8,11 +8,25 @@
 
 int main() {
   ProcessManager processManager;
+  
   processManager.createProcess( "p1", Undefined() );
   processManager.createProcess( "p2", Undefined(), 1 );
   // Wymóg dodania w ProcessManager::createProcess sprawdzania, czy nie
   // powtarza siê nazwa procesu
   processManager.createProcess( "p2", Undefined(), 2 );
-  const std::vector< Process >& processes = processManager.processes();
+  std::cout << "Vector of processes after adding three processes:\n\n";
+  std::vector< Process >& processes = processManager.processes();
+  std::for_each( processes.begin(), processes.end(), printProcessData );
+
+  auto changeStateToTerminated = []( Process& process ) {
+        process.setState( Process::State::Terminated );
+      };
+  std::for_each( processes.begin(), processes.end(),
+      changeStateToTerminated );
+  std::cout << "Vector of processes after changing these processes' state to terminated:\n\n";
+  std::for_each( processes.begin(), processes.end(), printProcessData );
+  
+  std::cout << "Vector of processes after removing all terminated processes:\n\n";
+  processManager.removeTerminatedProcesses();
   std::for_each( processes.begin(), processes.end(), printProcessData );
 }
