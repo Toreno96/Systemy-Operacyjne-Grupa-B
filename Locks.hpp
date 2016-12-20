31dce0461a243359178d195b8d3ac00ab2a388d0
Roski96 <oski0927@gmail.com>
2016-12-20 17:51:25 +0100

    Update Locks.hpp

--- Locks.hpp
+++ Locks.hpp
@@ -9,9 +9,10 @@ protected:
 	std::deque <std::reference_wrapper<Process>> waiting_process_list; //lista procesow czekajacych pod zamkiem
 	std::deque <std::string> blocking_IDs_list; //lista ID procesow czekajacych pod zamkiem, uzywana do weryfikacji procesu, ktory zajal zamek
 public:
-	int GetValue(); // ilosc procesow pod zamkiem
-	std::string GetProcessName(); //wypisuje id procesu pierwszego do zwolnienia
-	void Show_List_of_Processes(); // wyswietlenie calej listy procesow pod zamkiem
+	int getValue(); // ilosc procesow pod zamkiem
+	std::string getProcessName(); //zwraca id procesu pierwszego do zwolnienia
+	void displayProcessName();	//wypisuje id procesu pierwszego do zwolnienia
+	void showListOfProcesses(); // wyswietlenie calej listy procesow pod zamkiem
 	void lock(Process &process);   // metoda ryglujaca
 	void unlock(Process &process); // metoda otwierajaca zamek, otworzy jesli ID procesu zgadza sie z ID procesu, ktory zamknal zamek
 };
