48f4ea7b5762a38500f71b2c9c444299a4853dbb
Toreno96 <toreno96@gmail.com>
2016-12-23 01:48:21 +0100

    Uczyniono odpowiednie funkcje funkcjami const

--- src/Command.cpp
+++ src/Command.cpp
@@ -4,10 +4,10 @@
 Command::Command( const std::string& rawCommand ) {
   parseRawCommand( rawCommand );
 }
-const std::string& Command::name() {
+const std::string& Command::name() const {
   return name_;
 }
-const Command::tArguments Command::arguments() {
+const Command::tArguments Command::arguments() const {
   return arguments_;
 }
 void Command::parseRawCommand( const std::string& rawCommand ) {
--- src/Command.hpp
+++ src/Command.hpp
@@ -7,8 +7,8 @@ class Command {
   public:
     typedef std::vector< std::string > tArguments;
     Command( const std::string& rawCommand );
-    const std::string& name();
-    const tArguments arguments();
+    const std::string& name() const;
+    const tArguments arguments() const;
   private:
     void parseRawCommand( const std::string& rawCommand );
     std::string name_;
