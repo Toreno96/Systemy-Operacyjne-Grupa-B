e9ed4fcdee358175c28ee19a29f149702aed8434
Toreno96 <toreno96@gmail.com>
2017-01-08 23:22:10 +0100

    Zmiana poziomu dostępu do składowych-modułów

--- src/System.hpp
+++ src/System.hpp
@@ -15,17 +15,17 @@ class System {
     void run();
     void shutdown();
     void crash( const std::string& errorMessage );
+    CPU cpu_;
+    HardDrive hardDrive_;
+    ProcessManager processManager_;
+    Pipes pipes_;
+    Interpreter interpreter_;
   private:
     void displayIntro();
     void useInterpreter();
     void useShell();
     // Do usuniêcia w ostatecznej wersji
     void testRunLoop();
-    CPU cpu_;
-    HardDrive hardDrive_;
-    ProcessManager processManager_;
-    Pipes pipes_;
-    Interpreter interpreter_;
     Shell shell_;
     bool running_;
 };
